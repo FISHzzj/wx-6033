136372dcbdda130190b337ccceedb3cf42d31ebd
ZengXianJie <1003895046@qq.com>
2018-09-29 15:54:07 +0800

    商城小程序初始化

---
+++ pages/shop/caregory/index.js
@@ -0,0 +1,85 @@
+var t = getApp(), e = t.requirejs("core"), a = (t.requirejs("icons"), t.requirejs("jquery"));
+Page({
+	data : {
+		route : "category",
+		category : {},
+		icons : t.requirejs("icons"),
+		selector : 0,
+		advimg : "",
+		recommands : {},
+		level : 0,
+		back : 0,
+		child : {},
+		parent : {},
+    id:0
+	},
+	tabCategory : function (t) {
+		this.setData({
+			selector : t.target.dataset.id,
+			advimg : t.target.dataset.src,
+			child : t.target.dataset.child,
+			back : 0
+		}),
+		a.isEmptyObject(t.target.dataset.child) ? this.setData({
+			level : 0
+		}) : this.setData({
+			level : 1
+		})
+	},
+	cateChild : function (t) {
+		this.setData({
+			parent : t.currentTarget.dataset.parent,
+			child : t.currentTarget.dataset.child,
+			back : 1
+		})
+	},
+	backParent : function (t) {
+		this.setData({
+			child : t.currentTarget.dataset.parent,
+			back : 0
+		})
+	},
+	getCategory : function () {
+		var q = this;
+    var merchid = t.getCache("merchid")
+    if (merchid != "" || merchid != 0) {
+      wx.hideTabBar()
+     q.setData({
+        id: merchid
+      })
+      e.get("shop/get_category", { merchid: merchid }, function (e) {
+        q.setData({
+          category: e.category,
+          show: !0,
+          set: e.set,
+          advimg: e.set.advimg,
+          recommands: e.recommands,
+          child: e.recommands
+        })
+      })
+    }
+    else{
+      wx.showTabBar()
+      e.get("shop/get_category", {}, function (e) {
+        q.setData({
+          category: e.category,
+          show: !0,
+          set: e.set,
+          advimg: e.set.advimg,
+          recommands: e.recommands,
+          child: e.recommands
+        })
+      })
+    }
+	},
+  onShow: function () {
+    this.getCategory()
+	},
+	onShareAppMessage : function () {
+		return e.onShareAppMessage()
+	},
+
+   onLoad: function (options) {
+   
+  },
+})
